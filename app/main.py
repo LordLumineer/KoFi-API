ad1cfa4e2f3836c284e01c02087a3b41ff2c2795
Lord Lumineer <56554075+LordLumineer@users.noreply.github.com>
2024-12-01 23:34:44 -0500

    fix(database error (missing table)): :wrench: added the import of the table class to the alembic env file
    forgot to add the class of the table in the alembic .env file so it wasn't creating proper migration file.

--- app/main.py
+++ app/main.py
@@ -6,18 +6,19 @@ Compact API for Ko-fi donations.
 @author: Your Name (your.name@example.com)
 """
 from contextlib import asynccontextmanager
+import os
 from apscheduler.schedulers.background import BackgroundScheduler
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.exceptions import HTTPException
+from fastapi.encoders import jsonable_encoder
 from fastapi.routing import APIRoute
-# from starlette.middleware.cors import CORSMiddleware
+from starlette.middleware.cors import CORSMiddleware
 
 from app.api.router import api_router
 from app.core import models
 from app.core import db as database
 from app.core.config import settings, logger
 from app.core.db import remove_expired_transactions, run_migrations
-
-models.Base.metadata.create_all(bind=database.engine)
 
 app = FastAPI()
 
@@ -26,6 +27,9 @@ app = FastAPI()
 async def lifespan(app: FastAPI):  # pylint: disable=unused-argument, redefined-outer-name
     """ Lifespan hook to run on application startup and shutdown. """
     logger.info("Starting up...")
+    os.makedirs('./data', exist_ok=True)
+    # Database
+    models.Base.metadata.create_all(bind=database.engine)
     # Alembic
     run_migrations()
     # Scheduler
@@ -49,20 +53,36 @@ app = FastAPI(
     It provides a set of API endpoints for handling donations, exporting and importing the database, 
     and admin-specific operations like managing users and transactions.
     """,
-    version="1.0.2",
+    version="1.0.3",
     generate_unique_id_function=custom_generate_unique_id,
     lifespan=lifespan,
 )
 
 app.include_router(api_router)
 
-# # app.add_middleware(
-# #     CORSMiddleware,
-# #     allow_origins=["*"],
-# #     allow_credentials=True,
-# #     allow_methods=["*"],
-# #     allow_headers=["*"],
-# # )
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.exception_handler(Exception)
+def _debug_exception_handler(request: Request, exc: Exception):  # pragma: no cover   # pylint: disable=unused-argument
+    logger.critical(exc)
+    if isinstance(exc, HTTPException):
+        if exc.status_code != 500:
+            raise HTTPException(status_code=exc.status_code, detail=exc.detail)
+    raise HTTPException(
+        status_code=500,
+        detail=jsonable_encoder({
+            "error": str(exc),
+            "support": f"{settings.FRONTEND_URL}/support",
+            "contact": settings.CONTACT_EMAIL
+        })
+    )
 
 
 @app.get("/ping", tags=["DEBUG"])
